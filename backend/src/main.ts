1c1df0bff9a9c6bc669b1aa8ea671c3151287a61
DarwinGG06 <124894985+DarwinGG06@users.noreply.github.com>
2026-04-17 16:57:55 -0500

    Cambio de cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:4200'], credentials: true });
+  app.enableCors({ origin: '*', credentials: true });
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
