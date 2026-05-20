fc1c491bb12ad3cf56ec6324dfbbb2db56f68f12
DarwinGG06 <124894985+DarwinGG06@users.noreply.github.com>
2026-05-19 22:58:43 -0500

    Despliegue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*', credentials: true });
+  app.enableCors({ origin: 'https://taskmaster1-one.vercel.app', credentials: true });
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
